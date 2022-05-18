455ddce2bc290cd4f014531e2055c32205cfc814
Artaey <95622783+Artaey@users.noreply.github.com>
2022-05-18 11:15:24 +0200

    arrows

--- styleguide/style.js
+++ styleguide/style.js
@@ -1,8 +1,16 @@
 function showhide(elem){
-    let el = elem.getElementsByTagName("div")[0]
+    let el = elem.getElementsByTagName("div")[1];
     if(el.style.display == "none"){
-        el.style.display = "grid" 
+        el.style.display = "grid";
     }else{
-        el.style.display = "none"
+        el.style.display = "none";
     }
+    let arrow = elem.getElementsByTagName("p")[1];
+    if(arrow.innerHTML == "▽"){
+        arrow.innerHTML = "△";
+    }else{
+        arrow.innerHTML = "▽";
+    }
+    
+
 }
