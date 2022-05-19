7aae0ce369c46bc8885542d743975ed1722d7434
nuksnuks <davidwogelius@gmail.com>
2022-05-19 13:38:39 +0200

    heheh endnu en opdatering

--- login/opretbruger.js
+++ login/opretbruger.js
@@ -9,18 +9,15 @@ function gemData(){
   email = document.getElementById("email").value;
 
   sessionStorage.setItem('name', fornavn);
-  console.log(sessionStorage.getItem('name'));
-
   sessionStorage.setItem('lastname', efternavn);
-  console.log(sessionStorage.getItem('password'));
-
   sessionStorage.setItem('username', brugernavn);
-  console.log(sessionStorage.getItem('username'));
-
   sessionStorage.setItem('password', kode);
-  console.log(sessionStorage.getItem('password'));
-
   sessionStorage.setItem('email', email);
+
+  console.log(sessionStorage.getItem('password'));
+  console.log(sessionStorage.getItem('name'));
+  console.log(sessionStorage.getItem('username'));
+  console.log(sessionStorage.getItem('password'));
   console.log(sessionStorage.getItem('email'));
 
   document.getElementById('login-container').style.display = 'flex';
