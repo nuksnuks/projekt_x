0d6a9f8134bb2ab96de169a00184120524cc3c95
ASkafte <amaks98@hotmail.com>
2022-05-22 14:46:04 +0200

    rettelser

--- Popups/ftjeneste.js
+++ Popups/ftjeneste.js
@@ -9,17 +9,14 @@ let fjern2 = document.getElementById("fjern2");
 let ja = document.getElementById("ja");
 let ok2 = document.getElementById("ok2");
 
-// When the user clicks the button, open the modal
 fjern.onclick = function() {
   fjernm.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 nej.onclick = function() {
   fjernm.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == fjernm) {
     fjernm.style.display = "none";
@@ -31,13 +28,11 @@ ja.onclick = function() {
   fjern2.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 ok2.onclick = function() {
   fjern2.style.display = "none"
   fjernm.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == fjern2) {
     fjern2.style.display = "none"
