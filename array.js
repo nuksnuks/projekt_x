6d57b9dc7eeabd10d2047427dece15762e9cceb7
alvihelene <95623140+alvihelene@users.noreply.github.com>
2022-05-23 14:18:16 +0200

    Console.log fjernet og navne ændret lidt

--- array.js
+++ array.js
@@ -1,15 +1,14 @@
 
 
-let sum1 = document.getElementsByClassName("ab")[0].innerHTML;
-let sum2 = document.getElementsByClassName("ab")[1].innerHTML;
-let sum3 = document.getElementsByClassName("ab")[2].innerHTML;
-let allnumbers = [sum1, sum2, sum3];
-console.log(allnumbers)
+let tal1 = document.getElementsByClassName("ab")[0].innerHTML;
+let tal2 = document.getElementsByClassName("ab")[1].innerHTML;
+let tal3 = document.getElementsByClassName("ab")[2].innerHTML;
+let allnumbers = [tal1, tal2, tal3];
 
 let sum=0;
 for (let i =0;i<allnumbers.length;i++) {
 sum += parseInt(allnumbers[i])
 }
-console.log(sum)
+
 console.log(document.getElementsByClassName("item3")[0])
 document.getElementsByClassName("item3")[0].innerHTML += sum
