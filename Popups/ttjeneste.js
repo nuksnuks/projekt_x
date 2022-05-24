c08fdd7921bd6fd103727a3a8ec4496820bba6cc
ASkafte <amaks98@hotmail.com>
2022-05-24 11:55:08 +0200

    pop up til tilføj tjeneste færdig

--- Popups/ttjeneste.js
+++ Popups/ttjeneste.js
@@ -6,9 +6,6 @@ let tkonto = document.getElementById("tkonto");
 //modalerne der lukker popups//
 let luk = document.getElementById("luk");
 let ttjeneste = document.getElementById("ttjeneste");
-let tkonti = document.getElementById("tkonti");
-let button = document.getElementById("ok");
-let luk2 = document.getElementById("luk2");
 
 tilfoej.onclick = function () {
   ttprofil.style.display = "block";
@@ -23,22 +20,5 @@ ttjeneste.onclick = function() {
 window.onclick = function(event) {
   if (event.target == ttprofil) {
     ttprofil.style.display = "none";
-  }
-}
-//2nd modal//
-tkonto.onclick = function() {
-  tkonti.style.display = "block";
-  }
-
-ok.onclick = function() {
-  tkonti.style.display = "none"
-}
-luk2.onclick = function() {
-  tkonti.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == tkonti) {
-    tkonti.style.display = "none";
   }
 }
