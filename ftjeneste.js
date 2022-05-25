12aa2ef558a3dde5529c172f88749c194d09505f
Artaey <95622783+Artaey@users.noreply.github.com>
2022-05-25 13:17:13 +0200

    add fjern knap

---
+++ ftjeneste.js
@@ -0,0 +1,34 @@
+//Amanda//
+//modalerne der åbner popups//
+let fjernm = document.getElementById("fjernm");
+let fjern = document.getElementById("fjern");
+
+//modalerne der lukker popups//
+let nej = document.getElementById("nej");
+let fjern2 = document.getElementById("fjern2");
+let ja = document.getElementById("ja");
+let ok2 = document.getElementById("ok2");
+
+fjern.onclick = function() {
+  fjernm.style.display = "block";
+}
+
+nej.onclick = function() {
+  fjernm.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == fjernm) {
+    fjernm.style.display = "none";
+  }
+}
+
+//2nd modal//
+ja.onclick = function() {
+  fjern2.style.display = "block";
+}
+
+ok2.onclick = function() {
+  fjern2.style.display = "none"
+  fjernm.style.display = "none";
+}
